b90153d7af57c5ad4d5bcd11a715dc661310a4c4
345dvl <k.f.kntn@gmail.com>
2022-05-17 22:46:14 +0900

    :sparkles: サインアップページ追加

---
+++ pages/signup.tsx
@@ -0,0 +1,67 @@
+import { Button, InputLabel, TextField } from "@mui/material"
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth"
+import { NextPage } from "next"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useState } from "react"
+import { app } from "../lib/firebase"
+
+
+const Signup: NextPage = () => {
+  const router = useRouter()
+  const auth = getAuth(app)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    await createUserWithEmailAndPassword(auth, email, password)
+    router.push("/")
+  }
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value)
+  }
+  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value)
+  }
+
+  return (
+    <>
+      <h1>
+        サインアップ
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <InputLabel>メールアドレス</InputLabel>
+          <TextField
+            name="email"
+            type="email"
+            size="small"
+            onChange={handleChangeEmail}
+          />
+        </div>
+        <div>
+          <InputLabel>パスワード</InputLabel>
+          <TextField
+            name="password"
+            type="password"
+            size="small"
+            onChange={handleChangePassword}
+          />
+        </div>
+        <div>
+          <Button type="submit" variant="outlined">
+            登録
+          </Button>
+        </div>
+        <div>
+          <Link href={"/login"}>
+            <a>すでに登録している人はこちら</a>
+          </Link>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default Signup
